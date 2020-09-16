e6561ec68d8c9af58c80dca6a014439c4a9be7fe
Aditya Babu K C <71261653+abkc727@users.noreply.github.com>
2020-09-16 12:33:28 +0530

    Day2 updated

--- DAY 2/User.java
+++ DAY 2/User.java
@@ -1,10 +1,10 @@
 class User {
     public static void main(String[] args) {
         Savings s1 = new Savings(123456789L,"Aditya","Kerala","Calicut");
-        Savings s2 = new Savings(654421780L,"Akshay",3512345.21D,"Tamil Nadu","Vellore");
-        Savings s3 = new Savings(343437651L,"Hari", 43123211.1D, "Maharashta","Pune", "ASC123");
+        Savings s2 = new Savings(654421780L,"Akshay",35123.21D,"Tamil Nadu","Vellore");
+        Savings s3 = new Savings(343437651L,"Hari", 43121.1D, "Maharashta","Pune", "ASC123");
         Savings s4 = new Savings(907234112L,"Chandni","Kerala");
-        Savings s5 = new Savings(654444549L,"Akash",3434512.21D,"Telangana");
+        Savings s5 = new Savings(654444549L,"Akash",34342.21D,"Telangana");
 
         s1.display();
         s2.display();
