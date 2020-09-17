63433bbdab613a46ee36d43f6e3cf2f4de64829f
Aditya Babu K C <71261653+abkc727@users.noreply.github.com>
2020-09-17 17:36:21 +0530

    updated

--- DAY 3/MainBank.java
+++ DAY 3/MainBank.java
@@ -1,80 +1,41 @@
 public class MainBank {
     public static void main(String[] args) {
+        long AccountNumber = Long.parseLong(args[0]);
+        double depositamount = Double.parseDouble(args[1]);
+        Account acc = getAccount(AccountNumber);
 
-	System.out.println("------2nd Requirement--------");
-	long sourceAccNo = Long.parseLong(args[0]);
-	long destAccNo = Long.parseLong(args[1]);
-	double amount = Double.parseDouble(args[2]);
+        if(acc==null)
+        {
+            System.out.println("Invalid account number");
 
-	Account sourceAccount = getAccount(sourceAccNo);
-	Account destAccount = getAccount(destAccNo);
-
-	if(!accountCheck(sourceAccount) && !accountCheck(destAccount)){
-
-	System.out.println("------------------------------------------");
-	System.out.println("enter correct account details");
-	System.out.println("------------------------------------------");
-	return ;
-
-
-	}
-
-	System.out.println("------------------------------------------");
-
-	System.out.println("------Source account details-----");
-	sourceAccount.accountInformation();
-	System.out.println("-----Destination account details------");
-	destAccount.accountInformation();
-
-	System.out.println("------------------------------------------");
-	System.out.println("------Transferring Cash from source to dest-----");
-	sourceAccount.withdraw(amount);
-	destAccount.deposit(amount);
-
-	System.out.println("------------------------------------------");
-
-	System.out.println("------Source account details-----");
-	sourceAccount.accountInformation();
-	System.out.println("-----Destination account details------");
-	destAccount.accountInformation();
-	System.out.println("------------------------------------------");
-
-
-	
+        }
+        else
+        {
+            acc.accountInformation();
+            acc.deposit(depositamount);
+            acc.accountInformation();
+        }
+        
+        
     }
 
- 
+    // pass the accountNO and get the Account from Accounts[] 
     public static Account getAccount(long accountNo) {
         Account[] accounts = AccountsDB.getAccounts();
 
+        
 
-
+        
+        Account account = null;
         // get the account from accounts using the accountNo
-	for(Account account : accounts){
 
-		if(account.getAccountNumber() == accountNo)
-			return account;
+        for(int i=0;i<accounts.length;i++)
+            {
+                if(accounts[i].accountNumber==accountNo)
+                    account=accounts[i];
+                
+            }
 
-	}
-
-	return null;
+        return account;            
     }
-
-
-    public static Boolean accountCheck(Account account) {
-
-	if(account == null){
-		
-		System.out.println("------------------------------------------");
-		System.out.println("Sorry Account not found");
-		System.out.println("------------------------------------------");
-
-		return false;
-
-	}
-	return true;
-
-
-
-}
 }
